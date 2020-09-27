a288e0e261245449760b2d52b9d32e4116df9a34
Kamrin <kamrinkennedy@gmail.com>
2020-09-26 21:06:51 -0500

    bio container

--- src/containers/BioCardContainer.js
+++ src/containers/BioCardContainer.js
@@ -11,15 +11,8 @@ class BioCardContainer extends Component {
     }
 
     displayBio(){
-        // debugger;
-        // let actor = this.props.actors.actors.find( actor => actor.id === this.props.actorID)
-        // let { age, weight, height, eye_color, hair_color } = this.props.bioCard
         return (
             <div className='bioCard'>
-                {/* <h1>Bio Card</h1><br/>
-                <h3>Age: {age} | Weight: {weight} | Height: {height}</h3><br/>
-                <h3>Eye Color: {eye_color} | Hair Color: {hair_color}</h3><br/>
-                <BioCardForm bioCard={this.props.bioCard} actorID={this.props.actorID}/> */}
                 <BioCard bioCard={this.props.bioCard} actorID={this.props.actorID}/>
             </div>
         )
